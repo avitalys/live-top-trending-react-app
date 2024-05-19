ea517aef1e7441b1f7210a80ccf0d405d5ee0012
Avitalys <avitalyo52@gmail.com>
2024-05-19 22:20:25 +0300

    Fix queryClient setup

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,32 @@ import themes from "./themes";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CardsContainer from "./components/CardsContainer";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <ThemeProvider theme={themes.purple}>
-      {/* <img src="/vite.svg" alt="Vite logo" />
+      <QueryClientProvider client={queryClient}>
+        {/* <img src="/vite.svg" alt="Vite logo" />
       <div className="card">
         <Button $primary onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </Button>
       </div> */}
 
-      <Header />
-      <CardsContainer />
-      <Footer>
-        <p>&copy; 2024 avitalys/Live-Top-Trending App. All rights reserved.</p>
-      </Footer>
+        <Header />
+        <CardsContainer />
+        <Footer>
+          <p>
+            &copy; 2024 avitalys/Live-Top-Trending App. All rights reserved.
+          </p>
+        </Footer>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
